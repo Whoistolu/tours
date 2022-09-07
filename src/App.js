d074cc3e9b77451ac3a1f6df2d5721e7ad778cdf
Whoitolu <digittolu25@gmail.com>
2022-09-07 15:46:33 +0100

    Create Tours, Tour, Loading components wit some styles in the index.css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Loading from './components/Loading';
+import Tours from './components/Tours';
+const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
+  const [Loading, setLoading] = useState(true);
+  const [Tours, setTours] = useState([]);
+  if (Loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
   return (
-    <div>App</div>
+    <div>Tours Project</div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Tour.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Tour = () => {
+  return (
+    <div> Single Tour Component</div>
+  )
+};
+
+export default Tour;
