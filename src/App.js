41cd4b2071cd2cab47d5264afa7c41791f483e00
Whoitolu <digittolu25@gmail.com>
2022-09-19 10:47:55 +0100

    Update the index.js file

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Tours from './components/Tours';
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
-  const [Loading, setLoading] = useState(true);
+  const [loadings, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
   const fetchTours = async () => {
-    setLoading(true);
+    setLoading(false);
     const response = await fetch(url);
     const tours = await response.json();
     setTours(tours);
@@ -18,7 +18,7 @@ const App = () => {
     fetchTours();
   },[]);
 
-  if (Loading) {
+  if (loadings) {
     return (
       <main>
         <Loading tours={tours}/>
--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
   return (
